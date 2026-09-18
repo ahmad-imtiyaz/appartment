6f35a3d06ecd14bcbaa43d197a9f13648836a200
Dani Ahmad Izzul Haq <daniizzulhaq2002tkj@gmail.com>
2026-09-18 14:34:15 +0700

    perbaikan ddashboard worker

--- routes/web.php
+++ routes/web.php
@@ -314,6 +314,19 @@ Route::middleware(['auth', 'role:pekerja'])
 
         Route::post('/tasks/{serviceRequest}/complete', [TaskController::class, 'complete'])
             ->name('tasks.complete');
+
+        /*
+        | Profile
+        */
+
+        Route::get('/profile', [ProfileController::class, 'edit'])
+            ->name('profile.edit');
+
+        Route::patch('/profile', [ProfileController::class, 'update'])
+            ->name('profile.update');
+
+        Route::delete('/profile', [ProfileController::class, 'destroy'])
+            ->name('profile.destroy');
     });
 
 /*
